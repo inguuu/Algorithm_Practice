be543de0321dbb314697dd0ad694b5e435a6a812
inguuu <dlsrb8949@gmail.com>
2019-08-29 14:55:19 +0900

    setting 17140

---
+++ Baekjoon/Baekjoon/17140.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm> 
+#include<iostream>
+using namespace std;
+
+
+
+
+
+
+int main() {
+
+	const int MAX = 100;
+
+	int r, c, k;
+	int A[MAX][MAX];
+
+
+
+	if (i >= j) {
+
+	}
+	else if (i < j) {
+
+	}
+	
+
+
+}
