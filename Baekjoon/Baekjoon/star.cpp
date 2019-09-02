c450f0a4de6d2631124bc66b0407b8a4bc6ae894
inguuu <dlsrb8949@gmail.com>
2019-09-02 13:12:18 +0900

    star#4

---
+++ Baekjoon/Baekjoon/star.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	int n = a - 1;
+	int m = a - 1;
+	for (int i = 0; i < a; i++) {
+
+		for (int j = 0; j <= a * 2 - 1; j++) {
+			if (i == (a + 1) / 2 - 1) {
+				if (m <= j && j <= n) {
+					printf("*");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			else {
+
+				if (j == n || j == m) {
+					printf("*");
+				}
+				else {
+					printf(" ");
+				}
+			}
+
+		}
+		n++;
+		m--;
+		printf("\n");
+	}
+}
